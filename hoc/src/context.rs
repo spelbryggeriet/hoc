8f69364a1399bb1093497cde52750f5f6f7076c7
Hampus Lidin <hampuslidin@gmail.com>
2021-09-18 21:20:23 +0200

    Move `run_procedure` out of `Context` into a free function in "main.rs"

--- hoc/src/context.rs
+++ hoc/src/context.rs
@@ -1,19 +1,16 @@
 use std::{
     collections::HashMap,
     env,
-    fs::{self, File, OpenOptions},
+    fs::{self, File},
     io::{self, Seek, SeekFrom},
     mem,
+    ops::{Index, IndexMut},
     path::PathBuf,
 };
 
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
-use crate::{
-    procedure::{Halt, Procedure, ProcedureState},
-    Error, Result,
-};
-use hoclog::status;
+use crate::{procedure::ProcedureState, Error, Result};
 
 const ENV_HOME: &str = "HOME";
 
@@ -22,18 +19,23 @@ pub struct Context {
     proc_caches: HashMap<String, ProcedureCache>,
 }
 
-impl Context {
-    const CONTEXT_FILE_NAME: &'static str = "context.yaml";
-    const CONTEXT_DIR: &'static str = ".hoc";
+impl Index<&str> for Context {
+    type Output = ProcedureCache;
 
-    fn get_context_dir() -> Result<PathBuf> {
-        let home = env::var(ENV_HOME).map_err(|err| Error::environment(ENV_HOME, err))?;
-        let mut context_path = PathBuf::new();
-        context_path.push(home);
-        context_path.push(Self::CONTEXT_DIR);
-
-        Ok(context_path)
+    fn index(&self, index: &str) -> &Self::Output {
+        &self.proc_caches[index]
     }
+}
+
+impl IndexMut<&str> for Context {
+    fn index_mut(&mut self, index: &str) -> &mut Self::Output {
+        self.proc_caches.get_mut(index).unwrap()
+    }
+}
+
+impl Context {
+    pub const CONTEXT_FILE_NAME: &'static str = "context.yaml";
+    const CONTEXT_DIR: &'static str = ".hoc";
 
     pub fn load() -> Result<Self> {
         let mut context_path = Self::get_context_dir()?;
@@ -59,68 +61,24 @@ impl Context {
         }
     }
 
-    pub fn run_procedure<P: Procedure>(&mut self, mut proc: P) -> Result<()> {
-        let mut context_dir = Context::get_context_dir()?;
-        context_dir.push(Context::CONTEXT_FILE_NAME);
+    pub fn get_context_dir() -> Result<PathBuf> {
+        let home = env::var(ENV_HOME).map_err(|err| Error::environment(ENV_HOME, err))?;
+        let mut context_path = PathBuf::new();
+        context_path.push(home);
+        context_path.push(Self::CONTEXT_DIR);
 
-        let mut file = OpenOptions::new().write(true).open(context_dir)?;
-
-        if !self.proc_caches.contains_key(P::NAME) {
-            let state = P::State::INITIAL_STATE;
-            let description = state.description().to_owned();
-            self.proc_caches.insert(
-                P::NAME.to_string(),
-                ProcedureCache::new(
-                    &state,
-                    ProcedureStepDescription {
-                        index: 1,
-                        description,
-                    },
-                )?,
-            );
-            self.persist(&mut file)?;
-        }
-
-        let cache = &self.proc_caches[P::NAME];
-        if !cache.first_steps.is_empty() {
-            for proc_step in cache.first_steps.iter() {
-                status!(("[CACHED] Skipping step {}: {}", proc_step.index, proc_step.description) => ());
-            }
-        }
-
-        let mut state = cache.deserialize_state::<P::State>()?;
-        let mut index = cache.last_step.index;
-
-        loop {
-            let cache = self.proc_caches.get_mut(P::NAME).unwrap();
-            if let Some(inner_state) = state {
-                status!(("Step {}: {}", index, inner_state.description()) => {
-                    index += 1;
-                    let (description, new_state) = match proc.run(inner_state)? {
-                        Halt::Yield(inner_state) => {
-                            (inner_state.description().to_owned(), Some(inner_state))
-                        }
-                        Halt::Finish => (String::new(), None),
-                    };
-
-                    state = new_state;
-                    cache.serialize_state(&state)?;
-                    cache.push_step(ProcedureStepDescription { index, description });
-                    self.persist(&mut file)?;
-
-                    if state.is_none(){
-                        break;
-                    }
-                });
-            } else {
-                break;
-            };
-        }
-
-        Ok(())
+        Ok(context_path)
     }
 
-    fn persist(&self, file: &mut File) -> Result<()> {
+    pub fn is_procedure_cached(&self, name: &str) -> bool {
+        self.proc_caches.contains_key(name)
+    }
+
+    pub fn update_procedure_cache(&mut self, name: String, cache: ProcedureCache) {
+        self.proc_caches.insert(name, cache);
+    }
+
+    pub fn persist(&self, file: &mut File) -> Result<()> {
         file.set_len(0)?;
         file.seek(SeekFrom::Start(0))?;
         serde_yaml::to_writer(&*file, self)?;
@@ -130,42 +88,59 @@ impl Context {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct ProcedureCache {
+pub struct ProcedureCache {
     state: Option<String>,
     first_steps: Vec<ProcedureStepDescription>,
     last_step: ProcedureStepDescription,
 }
 
 impl ProcedureCache {
-    fn new<S: Serialize>(state: &S, step: ProcedureStepDescription) -> Result<Self> {
+    pub fn new<S: ProcedureState>(state: &S) -> Result<Self> {
         Ok(Self {
             state: Some(serde_json::to_string(state)?),
             first_steps: Vec::new(),
-            last_step: step,
+            last_step: ProcedureStepDescription {
+                index: 1,
+                description: S::INITIAL_STATE.description().to_owned(),
+            },
         })
     }
 
-    fn deserialize_state<S: DeserializeOwned>(&self) -> Result<Option<S>> {
+    pub fn cached_steps(&self) -> &[ProcedureStepDescription] {
+        &self.first_steps
+    }
+
+    pub fn current_step(&self) -> &ProcedureStepDescription {
+        &self.last_step
+    }
+
+    pub fn advance<S: ProcedureState>(&mut self, state: &Option<S>) -> Result<()> {
+        let proc_step = ProcedureStepDescription {
+            index: self.last_step.index + 1,
+            description: state
+                .as_ref()
+                .map(S::description)
+                .unwrap_or_default()
+                .to_owned(),
+        };
+
+        self.state = state.as_ref().map(serde_json::to_string).transpose()?;
+        self.first_steps
+            .push(mem::replace(&mut self.last_step, proc_step));
+        Ok(())
+    }
+
+    pub fn deserialize_state<S: DeserializeOwned>(&self) -> Result<Option<S>> {
         Ok(self
             .state
             .as_deref()
             .map(serde_json::from_str)
             .transpose()?)
     }
-
-    fn serialize_state<S: Serialize>(&mut self, state: &Option<S>) -> Result<()> {
-        self.state = state.as_ref().map(serde_json::to_string).transpose()?;
-        Ok(())
-    }
-
-    fn push_step(&mut self, step: ProcedureStepDescription) {
-        self.first_steps
-            .push(mem::replace(&mut self.last_step, step));
-    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct ProcedureStepDescription {
-    index: usize,
-    description: String,
+pub struct ProcedureStepDescription {
+    pub index: usize,
+    pub description: String,
 }
--- hoc/src/main.rs
+++ hoc/src/main.rs
@@ -1,8 +1,11 @@
-use std::result::Result as StdResult;
+use std::{fs::OpenOptions, result::Result as StdResult};
 
+use context::ProcedureCache;
+use hoclog::status;
+use procedure::{Procedure, ProcedureState};
 use structopt::StructOpt;
 
-use crate::{command::Command, context::Context, error::Error};
+use crate::{command::Command, context::Context, error::Error, procedure::Halt};
 
 mod command;
 mod context;
@@ -11,12 +14,54 @@ mod procedure;
 
 type Result<T> = StdResult<T, Error>;
 
+fn run_procedure<P: Procedure>(context: &mut Context, mut proc: P) -> Result<()> {
+    let mut context_dir = Context::get_context_dir()?;
+    context_dir.push(Context::CONTEXT_FILE_NAME);
+
+    let mut file = OpenOptions::new().write(true).open(context_dir)?;
+
+    if !context.is_procedure_cached(P::NAME) {
+        context.update_procedure_cache(
+            P::NAME.to_string(),
+            ProcedureCache::new(&P::State::INITIAL_STATE)?,
+        );
+        context.persist(&mut file)?;
+    }
+
+    let cache = &context[P::NAME];
+    for proc_step in cache.cached_steps() {
+        status!(("[CACHED] Skipping step {}: {}", proc_step.index, proc_step.description) => ());
+    }
+
+    let mut state = cache.deserialize_state::<P::State>()?;
+
+    loop {
+        let cache = &mut context[P::NAME];
+        if let Some(inner_state) = state {
+            let step = cache.current_step();
+            status!(("Step {}: {}", step.index, step.description) => {
+                state = match proc.run(inner_state)? {
+                    Halt::Yield(inner_state) => Some(inner_state),
+                    Halt::Finish => None,
+                };
+
+                cache.advance(&state)?;
+                context.persist(&mut file)?;
+            });
+        } else {
+            break;
+        };
+    }
+
+    Ok(())
+}
+
 fn main() {
     let wrapper = || -> Result<()> {
         let mut context = Context::load()?;
 
         match Command::from_args() {
-            Command::Flash(proc) => context.run_procedure(proc)?,
+            Command::Flash(proc) => run_procedure(&mut context, proc)?,
         }
         Ok(())
     };
