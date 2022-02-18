250f6cb62e28d4e25faf4bad7765fa0ee03abf74
Hampus Lidin <hampuslidin@gmail.com>
2022-02-18 09:44:25 +0100

    hoclog: Add timers to statuses

--- hoclog/src/log/status.rs
+++ hoclog/src/log/status.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    sync::{Arc, Mutex},
+    time::SystemTime,
+};
 
 use console::Style;
 
@@ -7,6 +10,7 @@ use crate::{context::PrintContext, log::LogType, prefix::PrefixPrefs};
 pub struct Status {
     print_context: Arc<Mutex<PrintContext>>,
     custom_label: Option<String>,
+    start_time: SystemTime,
 }
 
 impl Status {
@@ -14,6 +18,7 @@ impl Status {
         Status {
             print_context,
             custom_label: None,
+            start_time: SystemTime::now(),
         }
     }
 
@@ -24,6 +29,20 @@ impl Status {
 }
 impl Drop for Status {
     fn drop(&mut self) {
+        let blue = Style::new().blue();
+
+        let time = self.start_time.elapsed().unwrap().as_secs_f32();
+        let time_str = if time >= 60.0 {
+            let mins = (time / 60.0).floor();
+            let secs = (time % 60.0).floor();
+            blue.apply_to(format!("{mins:.0}m{secs:.0}s"))
+        } else if time >= 10.0 {
+            let secs = time.floor();
+            blue.apply_to(format!("{secs:.0}s"))
+        } else {
+            blue.apply_to(format!("{time:.2}s"))
+        };
+
         let mut print_context = self.print_context.lock().unwrap();
 
         let level = print_context.status_level();
@@ -31,12 +50,18 @@ impl Drop for Status {
         let mut line = String::new();
         if !print_context.failure {
             if let Some(label) = &self.custom_label {
-                line += &label;
+                line += &format!("({time_str}) {label}");
             } else {
-                line += &Style::new().green().apply_to("success").to_string();
+                line += &format!(
+                    "({time_str}) {}",
+                    Style::new().green().apply_to("success").to_string()
+                );
             }
         } else {
-            line += &Style::new().red().apply_to("failure").to_string();
+            line += &format!(
+                "({time_str}) {}",
+                Style::new().red().apply_to("failure").to_string()
+            );
             if level == 1 {
                 print_context.failure = false;
             }
