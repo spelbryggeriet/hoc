fefc2f608fe8807534e07d7ded2cdd9134cb660a
Hampus Lidin <hampuslidin@gmail.com>
2022-10-19 23:59:13 +0200

    Refactor progress macro to allow opt-in for scoped handles

--- hoc/src/action/run/init.rs
+++ hoc/src/action/run/init.rs
@@ -3,10 +3,15 @@ use std::net::IpAddr;
 use crate::{cidr::Cidr, prelude::*};
 
 #[throws(anyhow::Error)]
-pub async fn run(node_addresses: Cidr, gateway: IpAddr, _admin_username: String) {
-    progress!("Storing network information");
+pub async fn run(node_addresses: Cidr, gateway: IpAddr, admin_username: String) {
+    let net_info_progress = progress!("Storing network information");
 
     put!(node_addresses.ip_addr.to_string() => "network/start_address").await?;
     put!(node_addresses.prefix_len => "network/prefix_len").await?;
     put!(gateway.to_string() => "network/gateway").await?;
+
+    drop(net_info_progress);
+    progress_scoped!("Storing administrator user");
+
+    put!(admin_username.to_string() => "admin/username").await?;
 }
--- hoc/src/macros.rs
+++ hoc/src/macros.rs
@@ -1,21 +1,27 @@
 macro_rules! progress {
+    ($($args:tt)*) => {{
+        $crate::logger::progress(format!($($args)*))
+    }};
+}
+
+macro_rules! progress_scoped {
     ($($args:tt)*) => {
-        let __progress = $crate::logger::progress(format!($($args)*));
+        let __progress = progress!($($args)*);
     };
 }
 
 macro_rules! prompt {
-    ($($args:tt)*) => {
+    ($($args:tt)*) => {{
         $crate::prompt::PromptBuilder::new(
             $crate::util::from_arguments_to_cow(format_args!($($args)*)),
         )
-    };
+    }};
 }
 
 macro_rules! select {
-    ($($args:tt)*) => {
+    ($($args:tt)*) => {{
         $crate::prompt::select(format!($($args)*))
-    };
+    }};
 }
 
 macro_rules! put {
